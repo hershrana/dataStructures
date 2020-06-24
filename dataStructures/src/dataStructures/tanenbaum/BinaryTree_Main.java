d0504eaf3ce86bee693dcf9926b12c524b0ef9bc
hershrana <hershrana@gmail.com>
2020-06-24 09:53:00 +0530

    traversal v2 with common add child method

--- dataStructures/src/dataStructures/tanenbaum/BinaryTree_Main.java
+++ dataStructures/src/dataStructures/tanenbaum/BinaryTree_Main.java
@@ -23,9 +23,18 @@ class BinaryNode<T>
 		return outputString.toString();
 	}
 	
-	public String inOrderTraversal()
+	public void inOrderTraversal(BinaryNode<T> startWith, StringBuffer sb)
 	{
-		StringBuffer outputString = new StringBuffer(sb);
+		if (startWith == null)
+		{
+			 sb.append(pipe+"_");
+			 return;
+		}
+		inOrderTraversal(startWith.leftChild,sb);
+			sb.append(pipe+startWith.value);
+		inOrderTraversal(startWith.rightChild,sb);
+/*		
+		StringBuffer outputString = new StringBuffer("INO "+arrow+sb);
 		outputString.append("leftChild"+arrow+leftChild);
 		outputString.append(pipe);
 		outputString.append("root value "+arrow+value);
@@ -33,11 +42,23 @@ class BinaryNode<T>
 		outputString.append("rightChild"+arrow+rightChild);
 		outputString.append(eb);
 		return outputString.toString();
+*/
 	}
 	
-	public String preOrderTraversal()
+	public void preOrderTraversal(BinaryNode<T> startWith, StringBuffer sb)
 	{
-		StringBuffer outputString = new StringBuffer("PRE "+arrow+sb);
+		if (startWith == null)
+		{
+			 sb.append(pipe+"_");
+			 return;
+		}
+		sb.append(pipe+startWith.value);
+		
+		preOrderTraversal(startWith.leftChild,sb);
+			
+		preOrderTraversal(startWith.rightChild,sb);
+		/*
+		StringBuffer outputString = new StringBuffer("PRE  "+arrow+sb);
 		outputString.append("root value "+arrow+value);
 		outputString.append(pipe);
 		outputString.append("leftChild"+arrow+leftChild);
@@ -45,18 +66,32 @@ class BinaryNode<T>
 		outputString.append("rightChild"+arrow+rightChild);
 		outputString.append(eb);
 		return outputString.toString();
+		*/
 	}
 	
-	public String postOrderTraversal()
+	public void postOrderTraversal(BinaryNode<T> startWith, StringBuffer sb)
 	{
-		StringBuffer outputString = new StringBuffer(sb);
+		if (startWith == null)
+		{
+			 sb.append(pipe+"_");
+			 return;
+		}
+		
+		
+		postOrderTraversal(startWith.leftChild,sb);
+			
+		postOrderTraversal(startWith.rightChild,sb);
+		
+		sb.append(pipe+startWith.value);
+		/*
+		StringBuffer outputString = new StringBuffer("POST "+arrow+sb);
 		outputString.append("leftChild"+arrow+leftChild);
 		outputString.append(pipe);
 		outputString.append("rightChild"+arrow+rightChild);
 		outputString.append(pipe);
 		outputString.append("root value "+arrow+value);
 		outputString.append(eb);
-		return outputString.toString();
+		return outputString.toString(); */
 	}
 	
 	public Boolean valueEqualsTo(T compareValue)
@@ -90,6 +125,9 @@ class BinaryNode<T>
 	
 	public BinaryNode<T> getNodeFromValue(T findValue, BinaryNode<T> startWith)
 	{
+		if (startWith == null)
+			return null;
+		
 		if(startWith.valueEqualsTo(findValue))
 			return startWith;
 		
@@ -127,12 +165,12 @@ class BinaryNode<T>
 	}
 }
 
-class BinaryTree<DataType>
+class BinaryTree<T>
 {
-	BinaryNode<DataType> rootNode ;
-	public BinaryNode<DataType> startWithRoot(DataType value)
+	BinaryNode<T> rootNode ;
+	public BinaryNode<T> startWithRoot(T value)
 	{
-		rootNode  = new BinaryNode<DataType>();
+		rootNode  = new BinaryNode<T>();
 		rootNode.value = value;
 		return rootNode;
 	}
@@ -144,53 +182,136 @@ class BinaryTree<DataType>
 		return "";
 	}
 	
-	public void addLeftChild(DataType leftChildValue)
+	public void addLeftChild(T leftChildValue)
 	{
 		rootNode.addLeftChild(leftChildValue);
 	}
 	
-	public void addRightChild(DataType rightChildValue)
+	public void addRightChild(T rightChildValue)
 	{
 		rootNode.addRightChild(rightChildValue);
 	}
 	
-	public void addLeftChild(DataType leftChildValue, DataType atNodeValue)
+	public void addLeftChild(T atNodeValue,T leftChildValue)
 	{
-		rootNode.addLeftChild(leftChildValue);
+		rootNode.addLeftChild(leftChildValue,atNodeValue);
 	}
 	
-	public void addRightChild(DataType rightChildValue, DataType atNodeValue)
+	public void addRightChild(T atNodeValue, T rightChildValue)
 	{
-		rootNode.addRightChild(rightChildValue);
+		rootNode.addRightChild(rightChildValue,atNodeValue);
 	}
 	
-	public String inOrderTraversal()
+	public StringBuffer inOrderTraversal()
 	{
-		return rootNode.inOrderTraversal();
+		StringBuffer sb = new StringBuffer("IN  "+rootNode.arrow);
+		 rootNode.inOrderTraversal(rootNode,sb);
+		 return sb;
 	}
 	
-	public String preOrderTraversal()
+	public StringBuffer preOrderTraversal()
 	{
-		return rootNode.preOrderTraversal();
+		StringBuffer sb = new StringBuffer("PRE "+rootNode.arrow);
+		rootNode.preOrderTraversal(rootNode,sb);
+		return sb;
+	}
+
+	public StringBuffer postOrderTraversal()
+	{
+		StringBuffer sb = new StringBuffer("POST"+rootNode.arrow);
+		rootNode.postOrderTraversal(rootNode,sb);
+		return sb;
 	}
 	
-	public String postOrderTraversal()
+	public BinaryNode<T> getNodeFromValue(T findValue)
 	{
-		return rootNode.postOrderTraversal();
+		return rootNode.getNodeFromValue(findValue);
+	}
+	
+	public void addChild(T atNodeValue, String side ,T childValue)
+	{
+		if(side.toUpperCase().startsWith("R"))
+			rootNode.addRightChild(childValue, atNodeValue);
+		else
+			rootNode.addLeftChild(childValue, atNodeValue);
 	}
 	
 }
 public class BinaryTree_Main {
-	
+	static String right = "R";
+	static String left = "L";
 	public static void main(String args[])
+	{
+		CharTreeExample();
+		IntTreeExample();
+		CharTreeExample2();
+	}
+
+	static void CharTreeExample()
+	{
+		BinaryTree<Character> charTree = new BinaryTree<Character>();
+		charTree.startWithRoot('A');
+		charTree.addLeftChild('A', 'B');
+		charTree.addRightChild('A', 'C');
+		charTree.addLeftChild('B', 'D');
+		charTree.addRightChild('D', 'G');
+		
+		charTree.addLeftChild('C', 'E');
+		charTree.addRightChild('C', 'F');
+		
+		charTree.addLeftChild('E', 'H');
+		charTree.addRightChild('E', 'I');
+		
+		//charTree.addLeftChild('F', 'x');
+		//charTree.addRightChild('F', 'y');
+		
+		//StringBuffer sb = new StringBuffer();
+		System.out.println(charTree.preOrderTraversal()+"\n");
+		System.out.println(charTree.inOrderTraversal()+"\n");
+		//System.out.println(sb);
+		System.out.println(charTree.postOrderTraversal()+"\n");
+		
+	}
+	
+	static void CharTreeExample2()
+	{
+		BinaryTree<Character> charTree = new BinaryTree<Character>();
+		charTree.startWithRoot('A');
+		charTree.addChild('A', left, 'B');
+		charTree.addChild('B', left, 'C');
+		charTree.addChild('C', left, 'E');
+		charTree.addChild('E', right, 'I');
+		
+		charTree.addChild('C', right, 'F');
+		charTree.addChild('F', right, 'J');
+		
+		charTree.addChild('B', right, 'D');
+		charTree.addChild('D', left, 'G');
+		charTree.addChild('D', right, 'H');
+		
+		charTree.addChild('H', left, 'K');
+		charTree.addChild('H', right, 'L');
+
+		System.out.println(charTree.preOrderTraversal()+"\n");
+		System.out.println(charTree.inOrderTraversal()+"\n");
+		//System.out.println(sb);
+		System.out.println(charTree.postOrderTraversal()+"\n");
+		
+	}
+	
+	
+	static void IntTreeExample()
 	{
 		BinaryTree<Integer> binaryTreeInteger = new BinaryTree<Integer>();
 		binaryTreeInteger.startWithRoot(10);
 		binaryTreeInteger.addLeftChild(5);
 		binaryTreeInteger.addRightChild(15);
 		System.out.println(binaryTreeInteger.preOrderTraversal());
-		binaryTreeInteger.addLeftChild(10,1);
+		//System.out.println(binaryTreeInteger.getNodeFromValue(-1));
+		binaryTreeInteger.addLeftChild(15, 14);
+		System.out.println(binaryTreeInteger.preOrderTraversal());
+
+		binaryTreeInteger.addRightChild(15, 17);
 		System.out.println(binaryTreeInteger.preOrderTraversal());
 	}
-
 }
