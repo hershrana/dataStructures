ba940397c8b618b92a0707aa7f83cc31fb73c882
hershrana <hershrana@gmail.com>
2020-07-08 08:35:56 +0530

    bubble sort with improvement

--- dataStructures/src/dataStructures/gog/sorting/BubbleSortMain.java
+++ dataStructures/src/dataStructures/gog/sorting/BubbleSortMain.java
@@ -33,10 +33,57 @@ Third Pass:
 
 package dataStructures.gog.sorting;
 
-public class BubbleSortMain {
+import java.util.Arrays;
 
+public class BubbleSortMain {
+	public static void main(String args[])
+	{
+		/*
+		int arr[] = {5,1,4,2,8};
+		System.out.println(Arrays.toString(arr));
+		new BubbleSortAlgo().bubbleSort(arr);
+		System.out.println(Arrays.toString(arr));
+		*/
+		
+		
+		int arr1[] = {25,57,48,37,12,92,86,33};
+		System.out.println(Arrays.toString(arr1));
+		new BubbleSortAlgo().bubbleSort(arr1);
+		System.out.println(Arrays.toString(arr1));
+	}
 }
 
 class BubbleSortAlgo
 {
+	void bubbleSort(int arr[])
+	{
+		int arrLength = arr.length;
+		boolean improvement_Switched = true;
+		//for(int pass = 0 ; pass < (arrLength-1) ; pass ++) //without improvement
+		for(int pass = 0 ; pass < (arrLength-1) && improvement_Switched ; pass ++) //with improvement
+		{
+			improvement_Switched = false;
+			System.out.println("\t"+(pass+1)+"pass Entry -> "+Arrays.toString(arr));
+			for(int inner = 0 ; inner < (arrLength-1); inner ++)
+			{
+				boolean display_swapped = false;///just for display purpose
+				int display_swapIndex = 0 ; ///just for display purpose
+				System.out.print("\t\t"+Arrays.toString(arr));
+				if(arr[inner] > arr[inner+1])
+				{
+					int temp = arr[inner];
+					arr[inner] = arr[inner+1];
+					arr[inner+1] = temp;
+					display_swapped = true;
+					display_swapIndex = inner;
+					improvement_Switched = true;
+				}
+				if(display_swapped)
+				System.out.println("->"+Arrays.toString(arr)+ " swapped "+display_swapped+" as "+arr[display_swapIndex+1]+" > "+arr[display_swapIndex]);
+				else
+					System.out.println(" --> "+Arrays.toString(arr));
+			}
+			System.out.println("\t"+(pass+1)+"pass Exit  -> "+Arrays.toString(arr)+"\n");
+		}
+	}
 }
